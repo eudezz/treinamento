18fce3a98e572ab674c7625c1a3c4a5d6c8a0df7
eudez0 <eudeszanizete@gmail.com>
2026-02-03 11:21:33 -0300

    uo

--- idex2.js
+++ idex2.js
@@ -1,9 +1,18 @@
+ var Ientre= document.getElementById("iEnter")
+ var Saida= document.getElementById("saida")
+ 
  function   salvo(){
     var Ientre= document.getElementById("iEnter")
-    var Saida= document.getElementById("saida")
+    
     if (Ientre.value.trim()){
         Saida.innerHTML +=`<p>  <input type="checkbox">${Ientre.value}</p> `
         Saida.style.backgroundColor = ("#BC6C25")
+        Ientre.value=""
     }
 
 }
+function Limpar() {
+    if(Saida.lastElementChild)
+   { Saida.lastElementChild.remove()}
+}
+

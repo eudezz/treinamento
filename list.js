3cb16c6d8ade2272415df0d8c0eebbcdbff42065
eudez0 <eudeszanizete@gmail.com>
2026-03-25 21:10:50 -0300

    up

--- list.js
+++ list.js
@@ -1,5 +1,10 @@
 function saida() {
-    var text= document.getElementById("text")
+    var tex= document.getElementById("tex")
     var saida= document.getElementById("saida")
-    saida.innerHTML=`${text}`
+   
+
+    if (tex.value.trim() ) {
+        saida.innerHTML +=`<p >  <input type="checkbox">${tex.value} </p> `
+        tex.value=""
+    }
 }
